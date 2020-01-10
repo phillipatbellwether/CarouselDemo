516bddbac6d23ab60f73003ab372574afcdae5b5
Phillip Ko <phillipko@marcoss-mbp.lan>
2020-01-10 07:50:39 -0800

    first commit

--- src/index.js
+++ src/index.js
@@ -1,10 +1,44 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Carousel } from 'react-responsive-carousel';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function log(){
+    console.log("works");
+}
+
+
+function DemoCarousel(){
+      return (
+          <Carousel showIndicators={false} showArrows={false} showThumbs ={false} autoPlay={true}>
+              <div>
+                  <img src="assets/1.jpeg" />
+              </div>
+              <div>
+                  <img src="assets/2.jpeg" /> 
+              </div>
+              <div>
+                  <img src="assets/3.jpeg" /> 
+              </div>
+              <div>
+                  <img src="assets/4.jpeg" /> 
+              </div>
+              <div>
+                  <img src="assets/5.jpeg" /> 
+              </div>
+              <div>
+                  <img src="assets/6.jpeg" /> 
+              </div>
+          </Carousel>
+      );
+}
+
+ReactDOM.render(<DemoCarousel />, document.getElementById('root'));
+
+// ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
